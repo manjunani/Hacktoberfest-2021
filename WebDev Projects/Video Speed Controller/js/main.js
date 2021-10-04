480c5b93428eb7f46d157bc19ee267f9824bb39c
Ashutosh Padhy <ashutoshpadhy001@gmail.com>
2021-10-04 23:53:18 +0530

    Video Speed Controller
    A tool which controls the playback speed of the video by hovering mouse over the area.

---
+++ WebDev Projects/Video Speed Controller/js/main.js
@@ -0,0 +1,17 @@
+const speed = document.querySelector('.speed');
+const bar = speed.querySelector('.speed-bar');
+const video = document.querySelector('.flex');
+
+function handleMove(e) {
+    const y = e.pageY - this.offsetTop;
+    const percent = y / this.offsetHeight;
+    const min = 0.4;
+    const max = 4;
+    const height = Math.round(percent * 100) + '%';
+    const playbackRate = percent * (max - min) + min;
+    bar.style.height = height;
+    bar.textContent = playbackRate.toFixed(2) + '×';
+    video.playbackRate = playbackRate;
+  }
+
+speed.addEventListener('mousemove', handleMove);
