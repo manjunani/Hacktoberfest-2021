4fb1d4b8e072117977c593dfa2d69fd4b977f40c
TG2408 <tanmaygupta2408@gmail.com>
2021-10-06 22:01:39 +0530

    JS files updated

--- WebDev Projects/BackgroundGenerator/script.js
+++ WebDev Projects/BackgroundGenerator/script.js
@@ -1,7 +1,7 @@
-var css = document.querySelector("h3");
-var color1 = document.querySelector(".color1");
-var color2 = document.querySelector(".color2");
-var body = document.getElementById("gradient");
+const css = document.querySelector("h3");
+const color1 = document.querySelector(".color1");
+const color2 = document.querySelector(".color2");
+const body = document.getElementById("gradient");
 
 function setGradient() {
 	body.style.background = 
