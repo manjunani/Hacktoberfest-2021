299338339521a104e319fc60aab3c6969845a526
Aniket Pathak <aniketpathak028@gmail.com>
2021-10-08 09:22:37 +0530

    add an e-commerce website for coffee

---
+++ WebDev Projects/E-Commerce_Website_for_Coffee/index.js
@@ -0,0 +1,42 @@
+
+// mobile-menu
+const burgerIcon = document.querySelector('#burger-icon');
+const navbarMenu = document.querySelector('#navbar-links');
+
+burgerIcon.addEventListener('click', () => {
+    navbarMenu.classList.toggle('is-active');
+});
+
+// tabs
+const tabs = document.querySelectorAll('.tabs li');
+const tabContentBoxes = document.querySelectorAll('#tab-content > div');
+
+tabs.forEach(tab => {
+  tab.addEventListener('click', () => {
+    tabs.forEach(item => item.classList.remove('is-active'));
+    tab.classList.add('is-active');
+
+    const target = tab.dataset.target;
+    // console.log(target);
+    tabContentBoxes.forEach(box => {
+      if (box.getAttribute('id') === target) {
+        box.classList.remove('is-hidden');
+      } else {
+        box.classList.add('is-hidden');
+      }
+    })
+  })
+})
+
+// modal
+const signupButton = document.querySelector('#signup');
+const modalBg = document.querySelector('.modal-background');
+const modal = document.querySelector('.modal');
+
+signupButton.addEventListener('click', () => {
+  modal.classList.add('is-active');
+})
+
+modalBg.addEventListener('click', () => {
+  modal.classList.remove('is-active');
+})
