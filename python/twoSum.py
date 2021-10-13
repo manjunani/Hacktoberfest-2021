dce791c49b9f9e083a19732f04487ac492a7c87b
silvig06 <52558143+silvig06@users.noreply.github.com>
2021-10-13 12:43:53 +0530

    Create twoSum.py

---
+++ python/twoSum.py
@@ -0,0 +1,7 @@
+def twoSum(nums, target):
+  for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                sum = nums[i]+nums[j]
+                
+                if sum == target:
+                    return [i,j]
