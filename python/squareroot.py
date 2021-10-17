26ca9bd4bc56270bcc2b66d2a2fd7bc18f53e1b4
Safvan Melethil <90051965+SafvanMelethil@users.noreply.github.com>
2021-10-17 19:06:30 +0300

    square root

---
+++ python/squareroot.py
@@ -0,0 +1,9 @@
+# Python Square root program
+
+import math
+
+number = float(input(" Please Enter any numeric Value : "))
+
+squareRoot = math.pow(number, 0.5)
+
+print("The Square Root of a Given Number {0}  = {1}".format(number, squareRoot))
